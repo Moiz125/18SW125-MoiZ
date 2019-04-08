21ac13f8e2802984d35a533146912530dbae3857
Moiz125 <49184327+Moiz125@users.noreply.github.com>
2019-04-08 23:26:20 +0500

    Add files via upload

---
+++ OOP Lab (Tasks)/Lab#01/One.java
@@ -0,0 +1,26 @@
+class One
+{
+	public static void main(String[]nah)
+	{
+		String name="Mohammad MoiZ";
+		char grade='A';
+		char plus='+';
+		byte rollno=125;
+		short marks=430;
+		int fee=16000;
+		float percentage=86.6f;
+		double height=5.6;
+		boolean value=true;
+
+		System.out.println("Name:"+name);
+		System.out.println("RollNo:"+rollno);
+		System.out.println("Total Marks:"+marks);
+		System.out.println("Semester Fee:"+fee);
+		System.out.println("Result in percentage:"+percentage);
+		System.out.println("Height:"+height);
+		System.out.println("Result status:"+value);
+		System.out.println("Grade:"+grade+plus);
+
+
+	}
+}
---
+++ OOP Lab (Tasks)/Lab#01/Two.java
@@ -0,0 +1,13 @@
+class Two
+{
+	public static void main(String[]nah)
+	{
+		double num=75.25;
+		int num2=(int)num;
+		float num3=(float)(num-num2);
+
+		System.out.println("Original number:"+num);
+		System.out.println("Integeral part:"+num2);
+		System.out.println("Decimal part:"+num3);
+	}
+}
