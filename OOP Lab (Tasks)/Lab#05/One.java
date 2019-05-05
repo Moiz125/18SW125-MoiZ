482d9c40a8cb66ce915aff933d42cbf5ad52509c
Mohammad Moiz <49184327+Moiz125@users.noreply.github.com>
2019-05-05 11:22:47 +0500

    Add files via upload

---
+++ OOP Lab (Tasks)/Lab#05/One.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Seven
+{
+    public static void main(String[]nah)
+    {
+        Scanner x=new Scanner(System.in);
+        System.out.print("Enter an Integer:");
+        int a=x.nextInt();
+        System.out.print("Enter a String:");
+        String b=x.next();
+        System.out.print("Enter a float:");
+        float c=x.nextFloat();
+        System.out.println("Integer:"+a);
+        System.out.println("String:"+b);
+        System.out.println("Float:"+c);
+    }
+}
